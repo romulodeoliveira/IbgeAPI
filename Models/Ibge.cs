6f1f5b320ad0dbfe20c34134e520b202b41d8a75
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-17 23:40:52 -0300

    desenvolvimento dos models

---
+++ Models/Ibge.cs
@@ -0,0 +1,11 @@
+namespace IbgeApi.Models;
+
+class Ibge
+{
+    public Guid Id { get; set; } = new Guid();
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public string Email { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace IbgeApi.Models;
+
+class User
+{
+    public int Id { get; set; }
+    public string State { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
+
